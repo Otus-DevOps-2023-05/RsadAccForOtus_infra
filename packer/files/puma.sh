8da067b5afe98123891d0d5467187bc75e788e5f
RsadAccForOtus <ix7t2l7ol8if@mail.ru>
2023-07-07 17:49:42 +0300

    json exaple corrected 4, sh files corrected

--- packer/files/puma.sh
+++ packer/files/puma.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sleep 10
-sudo apt-get update
-sudo apt install -y git
+apt-get update
+sleep 10
+apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -2,8 +2,10 @@
 
 #Обновляем APT и устанавливаем Ruby и Bundler:
 sleep 10
-sudo apt update && sudo apt upgrade
-sudo apt install -y ruby-full ruby-bundler build-essential
+apt update
+sleep 10
+apt upgrade
+apt install -y ruby-full ruby-bundler build-essential
 
 #Проверить установку можно этими командами
 #ruby -v
